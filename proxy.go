e10fe8294a817f64540a55188ec7773187c5b329
tamboto2000 <tamboto2000@gmail.com>
2023-01-09 19:39:52 +0700

    Immediately write working proxy to a txt file

--- proxy.go
+++ proxy.go
@@ -2,10 +2,19 @@ package proxyfinder
 
 import (
 	"errors"
+	"fmt"
+	"os"
 	"sync"
 )
 
 func FindProxies(count int) ([]Proxy, error) {
+	f, err := os.Create("proxies.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
 	var proxies []Proxy
 	mx := new(sync.Mutex)
 	wg := new(sync.WaitGroup)
@@ -32,6 +41,7 @@ func FindProxies(count int) ([]Proxy, error) {
 
 			mx.Lock()
 			proxies = append(proxies, prox)
+			f.WriteString(fmt.Sprintf("http://%s:%s#%s#google=%v#cloudflare=%v\n", prox.Ip, prox.Port, prox.CountryCode, prox.Google, prox.Cloudflare))
 			mx.Unlock()
 		}(wg, mx)
 	}
